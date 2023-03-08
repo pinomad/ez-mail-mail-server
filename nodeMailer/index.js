be140e9c069a61734211811dc80d2eabfdf5cb86
Jimoon <roadzmoon76@gmail.com>
2023-03-09 08:51:32 +0900

    feat: 노드 메일러 기본 설정

---
+++ nodeMailer/index.js
@@ -0,0 +1,36 @@
+const nodemailer = require('nodemailer');
+
+const {
+  smtpPort,
+  smtpId,
+  smtpPassword,
+  smtpHost,
+  smtpDomain,
+} = require('../config');
+
+exports.sendMail = async function ({
+  emailTitle,
+  emailContent,
+  sender,
+  recipientAddress,
+  userName,
+}) {
+  const transporter = nodemailer.createTransport({
+    host: smtpHost,
+    port: smtpPort,
+    secure: smtpPort === '465',
+    auth: {
+      user: smtpId,
+      pass: smtpPassword,
+    },
+  });
+
+  const info = await transporter.sendMail({
+    from: `${sender || userName} <${smtpId}@${smtpDomain}>`,
+    to: recipientAddress,
+    subject: emailTitle,
+    html: emailContent,
+  });
+
+  return info;
+};
